cdb1edca12b4609a75abf48d3dbe3de6c6a3e6f6
Alejandro Casanova <4156075+elksa@users.noreply.github.com>
2025-01-14 22:29:18 +0100

    Tests result summary

--- .github/workflows/parse_test_results.sh
+++ .github/workflows/parse_test_results.sh
@@ -3,23 +3,34 @@
 # Directory containing the test result files
 TEST_RESULTS_DIR="test-results/app/build/test-results/testDebugUnitTest"
 
+# Initialize total counters
+total_tests=0
+total_skipped=0
+total_failures=0
+total_errors=0
+total_time=0
+
 # Iterate through each XML file in the directory
 for file in "$TEST_RESULTS_DIR"/*.xml; do
   # Extract the required attributes using xmllint
-  testsuite_name=$(xmllint --xpath 'string(/testsuite/@name)' "$file")
-  class_name=$(basename "$testsuite_name" | awk -F. '{print $NF}')
   tests=$(xmllint --xpath 'string(/testsuite/@tests)' "$file")
   skipped=$(xmllint --xpath 'string(/testsuite/@skipped)' "$file")
   failures=$(xmllint --xpath 'string(/testsuite/@failures)' "$file")
   errors=$(xmllint --xpath 'string(/testsuite/@errors)' "$file")
   time=$(xmllint --xpath 'string(/testsuite/@time)' "$file")
 
-  # Print the extracted information
-  echo "test class: $class_name"
-  echo "tests: $tests"
-  echo "skipped: $skipped"
-  echo "failures: $failures"
-  echo "errors: $errors"
-  echo "time: $time"
-  echo "-----------------------------"
+  # Sum the values to the total counters
+  total_tests=$((total_tests + tests))
+  total_skipped=$((total_skipped + skipped))
+  total_failures=$((total_failures + failures))
+  total_errors=$((total_errors + errors))
+  total_time=$(echo "$total_time + $time" | bc)
 done
+
+# Print the total amounts
+echo "Tests execution summary:"
+echo "Tests executed: $total_tests"
+echo "Tests skipped: $total_skipped"
+echo "Tests failed: $total_failures"
+echo "Total errors: $total_errors"
+echo "Total time: $total_time"
